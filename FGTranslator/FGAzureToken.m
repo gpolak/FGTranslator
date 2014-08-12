b81eef3675d747741cfe3a7485b8352885b64c9c
George Polak <george.polak@gmail.com>
2014-08-12 19:47:30 -0400

    adding Azure token files

---
+++ FGTranslator/FGAzureToken.m
@@ -0,0 +1,47 @@
+//
+//  FGAzureToken.m
+//  Fargate
+//
+//  Created by George Polak on 10/9/13.
+//
+//
+
+#import "FGAzureToken.h"
+
+@interface FGAzureToken ()
+
+@property (nonatomic) NSString *token;
+@property (nonatomic) NSDate *expiry;
+
+@end
+
+
+@implementation FGAzureToken
+
+- (id)initWithToken:(NSString *)token expiry:(NSDate *)expiry
+{
+    self = [super init];
+    if (self)
+    {
+        self.token = token;
+        self.expiry = expiry;
+    }
+    
+    return self;
+}
+
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"token:%@ expiry:%@", self.token, self.expiry];
+}
+
+- (BOOL)isValid
+{
+    if (!self.expiry)
+        return NO;
+    
+    return [self.expiry timeIntervalSinceNow] > 0;
+}
+
+
+@end
