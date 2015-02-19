07016fbe0b2a4975225417debbf0f7a6cef91e1d
George Polak <george.polak@gmail.com>
2015-02-18 19:05:23 -0500

    using demo credentials; fixing locale bug

--- FGTranslatorDemo/FGTranslatorDemo/ViewController.m
+++ FGTranslatorDemo/FGTranslatorDemo/ViewController.m
@@ -11,10 +11,6 @@
 #import "FGTranslator.h"
 #import "SVProgressHUD.h"
 
-static NSString *const GOOGLE_API_KEY = nil;
-static NSString *const BING_CLIENT_ID = nil;
-static NSString *const BING_CLIENT_SECRET = nil;
-
 @interface ViewController ()
 
 @property (nonatomic, weak) IBOutlet UITextView *textView;
@@ -36,21 +32,41 @@ static NSString *const BING_CLIENT_SECRET = nil;
     [FGTranslator flushCredentials];
 }
 
+- (FGTranslator *)translator {
+    /*
+     * using Bing Translate
+     *
+     * Note: The client id and secret here is very limited and is included for demo purposes only.
+     * You must use your own credentials for production apps.
+     */
+    FGTranslator *translator = [[FGTranslator alloc] initWithBingAzureClientId:@"fgtranslator-demo" secret:@"GrsgBiUCKACMB+j2TVOJtRboyRT8Q9WQHBKJuMKIxsU="];
+    
+    // or use Google Translate
+    
+    // using Google Translate
+    // translator = [[FGTranslator alloc] initWithGoogleAPIKey:@"your_google_key"];
+    
+    return translator;
+}
+
+- (NSLocale *)currentLocale {
+    NSLocale *locale = [NSLocale currentLocale];
+#if TARGET_IPHONE_SIMULATOR
+    // handling Apple bug
+    // http://stackoverflow.com/a/26769277/211692
+    return [NSLocale localeWithLocaleIdentifier:[locale localeIdentifier]];
+#else
+    return locale;
+#endif
+}
+
 - (IBAction)translate:(UIButton *)sender
 {
     [SVProgressHUD show];
     
     [self.textView resignFirstResponder];
     
-    FGTranslator *translator;
-    
-    // using Google Translate
-    translator = [[FGTranslator alloc] initWithGoogleAPIKey:GOOGLE_API_KEY];
-    
-    // using Bing Translate
-    // translator = [[FGTranslator alloc] initWithBingAzureClientId:BING_CLIENT_ID secret:BING_CLIENT_SECRET];
-    
-    [translator translateText:self.textView.text
+    [self.translator translateText:self.textView.text
                    completion:^(NSError *error, NSString *translated, NSString *sourceLanguage)
     {
          if (error)
@@ -61,7 +77,7 @@ static NSString *const BING_CLIENT_SECRET = nil;
          }
          else
          {
-             NSString *fromLanguage = [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:sourceLanguage];
+             NSString *fromLanguage = [[self currentLocale] displayNameForKey:NSLocaleIdentifier value:sourceLanguage];
              
              UIAlertView *alert = [[UIAlertView alloc] initWithTitle:fromLanguage ? [NSString stringWithFormat:@"from %@", fromLanguage] : nil
                                                              message:translated
@@ -81,15 +97,8 @@ static NSString *const BING_CLIENT_SECRET = nil;
     
     [self.textView resignFirstResponder];
     
-    FGTranslator *translator;
     
-    // using Google Translate
-    translator = [[FGTranslator alloc] initWithGoogleAPIKey:GOOGLE_API_KEY];
-    
-    // using Bing Translate
-    // translator = [[FGTranslator alloc] initWithBingAzureClientId:BING_CLIENT_ID secret:BING_CLIENT_SECRET];
-    
-    [translator detectLanguage:self.textView.text completion:^(NSError *error, NSString *detectedSource, float confidence)
+    [self.translator detectLanguage:self.textView.text completion:^(NSError *error, NSString *detectedSource, float confidence)
     {
         if (error)
         {
@@ -99,7 +108,7 @@ static NSString *const BING_CLIENT_SECRET = nil;
         }
         else
         {
-            NSString *fromLanguage = [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:detectedSource];
+            NSString *fromLanguage = [[self currentLocale] displayNameForKey:NSLocaleIdentifier value:detectedSource];
             
             NSString *confidenceMessage = confidence == FGTranslatorUnknownConfidence
                 ? @"unknown confidence"
@@ -123,15 +132,7 @@ static NSString *const BING_CLIENT_SECRET = nil;
     
     [self.textView resignFirstResponder];
     
-    FGTranslator *translator;
-    
-    // using Google Translate
-    translator = [[FGTranslator alloc] initWithGoogleAPIKey:GOOGLE_API_KEY];
-    
-    // using Bing Translate
-    // translator = [[FGTranslator alloc] initWithBingAzureClientId:BING_CLIENT_ID secret:BING_CLIENT_SECRET];
-    
-    [translator supportedLanguages:^(NSError *error, NSArray *languageCodes)
+    [self.translator supportedLanguages:^(NSError *error, NSArray *languageCodes)
     {
         if (error)
         {
@@ -142,11 +143,11 @@ static NSString *const BING_CLIENT_SECRET = nil;
         else
         {
             NSMutableString *languageMessage = [NSMutableString new];
-            NSLocale *locale = [NSLocale currentLocale];
+            NSLocale *locale = [self currentLocale];
             for (NSString *code in languageCodes)
                 [languageMessage appendFormat:@"%@\n", [locale displayNameForKey:NSLocaleIdentifier value:code]];
            
-            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%i Supported Languages", languageCodes.count]
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%ld Supported Languages", (long)languageCodes.count]
                                                             message:languageMessage
                                                            delegate:nil
                                                   cancelButtonTitle:@"OK"
